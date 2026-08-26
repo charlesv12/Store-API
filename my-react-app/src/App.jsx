6bbdd2192d452ab195962a1ad184f5e887dc7aed
charlesv12 <charles.villacruz@gmail.com>
2026-08-26 09:49:57 +0800

    updated syntax in app and main

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -17,7 +17,7 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/products/:id" element={<ProductDetail />} />
+      <Route path="/products/:id" element={<ProductDetail products={products} />} />
     </Routes>
   );
 }
