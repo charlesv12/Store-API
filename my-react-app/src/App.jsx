a3d8bea5d36c03d468b9a61715d69482921a1597
charlesv12 <charles.villacruz@gmail.com>
2026-08-26 08:40:42 +0800

    initialize 3 product cards

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,20 +1,31 @@
 import { useState } from 'react'
 import NavBar from './layouts/NavBarLayout.jsx'
 import FooterLayout from './layouts/FooterLayout.jsx'
+import ProductCard from './components/ProductCard';
 import './App.css'
 
-function App() {
+const products = [
+  { id: 1, name: 'Wireless Mouse', price: 599 },
+  { id: 2, name: 'Mechanical Keyboard', price: 2499 },
+  { id: 3, name: 'USB-C Hub', price: 899 },
+  ];
+
+export default function App() {
   return (
-    <div className="app-container">
-      <NavBar />
-      
-      <main className="main-content">
-        {/* Main page content goes here */}
-      </main>
-
-      <FooterLayout />
+    <div className="grid">
+    {products.map((product) => (
+    <ProductCard key={product.id} product={product} />
+    ))}
     </div>
-  )
+    
+  );
 }
-
-export default App
+    // src/components/ProductCard.jsx
+export default function ProductCard({ product }) {
+  return (
+    <div className="card">
+    <h3>{product.name}</h3>
+    <p>₱{product.price}</p>
+    </div>
+  );
+}
