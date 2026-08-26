d1916e529325ac6068bc7b5b9d7266868a734cce
charlesv12 <charles.villacruz@gmail.com>
2026-08-26 09:30:57 +0800

    updated syntaxes for routing

--- my-react-app/src/components/ProductCard.jsx
+++ my-react-app/src/components/ProductCard.jsx
@@ -1,13 +1,11 @@
 import { useState } from 'react';
 import styles from './ProductCard.module.css';
 import { Link } from 'react-router-dom';
-<div className={styles.card}></div>
 
 export default function ProductCard({ product }) {
     const [quantity, setQuantity] = useState(1);
     return (
         <div className={styles.card}>
-            <Link to={`/products/${product.id}`}>View Details</Link>
             <h3>{product.name}</h3>
             <p>₱{product.price}</p>
             <button onClick={() => setQuantity((q) => Math.max(1, q - 1))}>-</button>
