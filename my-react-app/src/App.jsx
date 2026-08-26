208617bd3e19ac07bb9f1b3eb35a84cb321d75bc
charlesv12 <charles.villacruz@gmail.com>
2026-08-26 09:07:48 +0800

    Created a router to connect home page and product detail page

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import NavBar from './layouts/NavBarLayout.jsx'
 import FooterLayout from './layouts/FooterLayout.jsx'
 import ProductCard from './components/ProductCard';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import ProductDetail from './pages/ProductDetail';
 import './App.css'
 
 const products = [
@@ -12,10 +15,9 @@ const products = [
 
 export default function App() {
   return (
-    <div className="grid">
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/products/:id" element={<ProductDetail />} />
+    </Routes>
   );
 }
